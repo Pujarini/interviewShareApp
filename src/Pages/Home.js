bb07877e7ba7cfc05af6674a61310fd407154ea6
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2022-10-06 13:03:28 +0530

    not required files modified

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import HeroSection from "../components/HeroSection";
-import HowPage from "../components/HowPage";
+// import HowPage from "../components/HowPage";
 import Navbar from "../components/Navbar";
 import ShareExperiences from "../components/ShareExperiences";
 // import Team from "../components/Team";
